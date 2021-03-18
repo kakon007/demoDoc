66fac2b6da3705fe4fcada5879356aa4e05fa4b9
jahid-ish <jahid@ishraak.com>
2021-03-18 13:17:04 +0600

    Sign In and appointment prompt

--- lib/features/dashboard/view/dash_board_screen.dart
+++ lib/features/dashboard/view/dash_board_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:hexcolor/hexcolor.dart';
 import 'package:myhealthbd_app/features/auth/sign_in_screen.dart';
+import 'package:myhealthbd_app/features/constant.dart';
+import 'package:myhealthbd_app/features/custom_dialog_box.dart';
 import 'package:myhealthbd_app/main_app/resource/strings_resource.dart';
 import 'package:myhealthbd_app/main_app/views/widgets/custom_card_pat.dart';
 import 'package:myhealthbd_app/main_app/views/widgets/custom_card_view.dart';
@@ -80,22 +82,143 @@ class _DashboardScreenState extends State<DashboardScreen> {
                   padding: const EdgeInsets.only(right:10),
                   child: GestureDetector(
                     onTap: (){
-                      Navigator.push(context, PageRouteBuilder(
-                        transitionDuration: Duration(seconds: 1),
-                        transitionsBuilder: (context, animation, secondaryAnimation, child) {
-                          var begin = Offset(0, 1.0);
-                          var end = Offset.zero;
-                          var curve = Curves.easeInOut;
+                      // Navigator.push(context, PageRouteBuilder(
+                      //   transitionDuration: Duration(seconds: 1),
+                      //   transitionsBuilder: (context, animation, secondaryAnimation, child) {
+                      //     var begin = Offset(0, 1.0);
+                      //     var end = Offset.zero;
+                      //     var curve = Curves.easeInOut;
+                      //
+                      //     var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+                      //
+                      //     return SlideTransition(
+                      //       position: animation.drive(tween),
+                      //       child: child,
+                      //     );
+                      //   },
+                      //   pageBuilder: (context, animation, secondaryAnimation) => SignIn(),
+                      // ));
 
-                          var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+                      showGeneralDialog(
+                        barrierLabel: "Label",
+                        barrierDismissible: true,
+                        barrierColor: Colors.black.withOpacity(0.5),
+                        transitionDuration: Duration(milliseconds: 700),
+                        context: context,
+                        pageBuilder: (context, anim1, anim2) {
+                          return Stack(
+                            children:[
+                               Align(
+                                alignment: Alignment.bottomCenter,
+                                child: Material(
+                                  type: MaterialType.transparency,
+                                  child: Container(
+                                    height: 200,
+                                   // child: SizedBox.expand(child: FlutterLogo()),
+                                    //margin: EdgeInsets.only(bottom: 50, left: 12, right: 12),
+                                    decoration: BoxDecoration(
+                                      //color: HexColor('#f9f2f3'),
+                                      gradient: LinearGradient(
+                                        begin: Alignment.topCenter,
+                                        end: Alignment.bottomCenter,
+                                        colors: [
+                                          HexColor('#fdf0f2'),
+                                          HexColor('#FFFFFF')
+                                        ],
+                                        tileMode: TileMode.repeated,
+                                      ),
+                                      borderRadius: radius,
+                                    ),
+                                    child: Padding(
+                                      padding: const EdgeInsets.only(top:70.0),
+                                      child: Center(
+                                        child: Column(
+                                          children: [
+                                            Text("To access your Patient portal,",style:TextStyle(fontSize: 18,color: Colors.black)),
+                                            SizedBox(height: 5,),
+                                            Text("Sign In required.",style:TextStyle(fontSize: 20,color: Colors.black,fontWeight: FontWeight.w500)),
+                                            SizedBox(height: 10,),
+                                            Padding(
+                                              padding: const EdgeInsets.only(left:40.0),
+                                              child: Row(
+                                                children: [
+                                                  GestureDetector(
+                                                    onTap:(){
+                                                      Navigator.pop(context);
+                                                    },
+                                                    child: Material(
+                                                      elevation: 0,
+                                                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8),side: BorderSide(color:HexColor('#354291') )),
+                                                      color: Colors.white,
+                                                      child: SizedBox(
+                                                        height: 50,
+                                                        width: 150,
+                                                        child: Center(
+                                                          child: Padding(
+                                                            padding: const EdgeInsets.all(8.0),
+                                                              child: Text(
+                                                                "Cancel",
+                                                                style: TextStyle(color: HexColor('#354291'),fontWeight: FontWeight.w500,fontSize: 15),
+                                                              ),
 
+                                                          ),
+                                                        ),
+                                                      ),
+                                                    ),
+                                                  ),
+                                                  SizedBox(width: 15,),
+                                                  Material(
+                                                    elevation: 0,
+                                                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                                                    color: HexColor('#354291'),
+                                                    child: SizedBox(
+                                                      height: 50,
+                                                      width: 150,
+                                                      child: Center(
+                                                        child: Padding(
+                                                          padding: const EdgeInsets.all(8.0),
+                                                          child: Text(
+                                                           "Continue",
+                                                            style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold),
+                                                          ),
+                                                        ),
+                                                      ),
+                                                    ),
+                                                  ),
+                                                ],
+                                              ),
+                                            )
+                                          ],
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+
+                              Positioned(
+                                top: MediaQuery.of(context).size.height/1.5,
+                                left:100,
+                                right:100,
+                                child: CircleAvatar(
+                                  backgroundColor: Colors.transparent,
+                                  radius: Constants.avatarRadius,
+                                  child: ClipRRect(
+                                      borderRadius: BorderRadius.all(Radius.circular(Constants.avatarRadius)),
+                                      child: Image.asset("assets/icons/sign_in_prompt.png")
+                                  ),
+                                ),
+                              ),
+                          ],
+                          );
+                        },
+                        transitionBuilder: (context, anim1, anim2, child) {
                           return SlideTransition(
-                            position: animation.drive(tween),
+                            position: Tween(begin: Offset(0, 2), end: Offset(0, 0)).animate(anim1),
                             child: child,
                           );
                         },
-                        pageBuilder: (context, animation, secondaryAnimation) => SignIn(),
-                      ));
+                      );
                     },
                     child: Row(
                       children: [
@@ -120,8 +243,8 @@ class _DashboardScreenState extends State<DashboardScreen> {
             ),
             drawer: Drawer(),
             body: SlidingUpPanel(
-              minHeight: 500,
-        maxHeight: 700,
+              minHeight: 480,
+        maxHeight: 710,
         isDraggable: true,
         //backdropEnabled: true,
               borderRadius: radius,

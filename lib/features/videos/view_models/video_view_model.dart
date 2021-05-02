3f12550cde51566a1ae60ad685c707f7935782a4
jahid-ish <jahid@ishraak.com>
2021-05-03 03:09:49 +0600

    Video pagination issue fixed

--- lib/features/videos/view_models/video_view_model.dart
+++ lib/features/videos/view_models/video_view_model.dart
@@ -1,4 +1,5 @@
 
+import 'package:dartz/dartz.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:myhealthbd_app/features/videos/models/channel_info_model.dart';
 import 'package:myhealthbd_app/features/videos/repositories/channel_Info_repository.dart';
@@ -12,6 +13,11 @@ class VideoViewModel extends ChangeNotifier{
   DateTime _lastFetchTime;
   bool _isFetchingMoreData = false;
   bool _isFetchingData = false;
+  bool _hasMoreData = false;
+  get logger => null;
+  String nextPageToken='';
+  int totalData=0;
+
   int _page = 1;
 
   Future<void> refresh(){
@@ -28,20 +34,74 @@ class VideoViewModel extends ChangeNotifier{
     }
     _isFetchingData = true;
     _lastFetchTime = DateTime.now();
-    var res = await VideoInfoRepository().getVideoInfo();
+    var res = await VideoInfoRepository.getVideoInfo();
     notifyListeners();
     _videoList.clear();
     res.fold((l) {
       _appError = l;
-      _isFetchingMoreData = false;
+      _isFetchingData = false;
       notifyListeners();
     }, (r) {
-      _isFetchingMoreData = false;
+      _isFetchingData = false;
       _videoList.addAll(r.dataList);
       notifyListeners();
     });
     print("VideoTileFromVM:::::::"+_videoList.first.snippet.title);
   }
+
+
+  // getMoreData() async {
+  //   print("Calling from Video getMoreData:::::");
+  //   print("HasMoreData ${hasMoreData}");
+  //   print("isFetchingMoreData ${isFetchingMoreData}");
+  //   print("isFetchingData ${isFetchingData}");
+  //   if (!isFetchingMoreData && !isFetchingData && hasMoreData) {
+  //
+  //    //_pageCount++;
+  //     isFetchingMoreData = true;
+  //     Either<AppError, VideoListM> result =
+  //     await VideoInfoRepository.getVideoInfo(pageToken:"CAUQAA");
+  //     return result.fold((l) {
+  //       isFetchingMoreData= false;
+  //       hasMoreData = false;
+  //       logger.i(l);
+  //       notifyListeners();
+  //       return false;
+  //     }, (r) {
+  //       hasMoreData = r.moreData??false;
+  //       isFetchingMoreData = false;
+  //       _videoList.addAll(r.dataList);
+  //       notifyListeners();
+  //       return true;
+  //     });
+  //   }
+  // }
+
+ // String _nextPageToken;
+  getMoreData(String pagrToken) async {
+    Either<AppError, VideoListM> result =
+        await VideoInfoRepository.getVideoInfo(pageToken:pagrToken==''?nextPageToken:pagrToken);
+
+    //_nextPageToken = res.nextPageToken;
+    //_videoList.clear();
+    return result.fold((l) {
+      isFetchingMoreData= false;
+      hasMoreData = false;
+      logger.i(l);
+      notifyListeners();
+      return false;
+    }, (r) {
+      totalData=r.maxData;
+      nextPageToken=r.moreData;
+      _videoList.addAll(r.dataList);
+      print("MoreVideoList From Youtube:::"+_videoList.toString());
+      print("MoreVideoList From Youtube Lenth:::"+_videoList.length.toString());
+      print("MoreVideoList From Youtube Token:::"+r.moreData);
+      notifyListeners();
+      return true;
+    });
+  }
+
 
   AppError get appError => _appError;
 
@@ -51,10 +111,21 @@ class VideoViewModel extends ChangeNotifier{
 
   bool get isFetchingMoreData => _isFetchingMoreData;
 
-  // bool get hasMoreData => _hasMoreData;
-  //
-  // bool get shouldFetchMoreData =>
-  //     _hasMoreData && !_isFetchingData && !_isFetchingMoreData;
+  set isFetchingMoreData(bool value) {
+    _isFetchingMoreData = value;
+    notifyListeners();
+  }
+
+
+
+  bool get hasMoreData => _hasMoreData;
+
+  set hasMoreData(bool value) {
+    _hasMoreData = value;
+    notifyListeners();
+  }
+
+
 
   bool get shouldShowPageLoader =>
       _isFetchingData && _videoList.length == 0;

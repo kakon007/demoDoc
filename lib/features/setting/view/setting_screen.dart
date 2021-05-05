5c7512d9b09fce3f892bb890192aadfe89b5be71
jahid-ish <jahid@ishraak.com>
2021-05-05 15:36:56 +0600

    App version added

--- lib/features/setting/view/setting_screen.dart
+++ lib/features/setting/view/setting_screen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hexcolor/hexcolor.dart';
+import 'package:myhealthbd_app/features/setting/view/wedgets/about_us_screen.dart';
 import 'package:myhealthbd_app/main_app/resource/colors.dart';
+import 'package:myhealthbd_app/main_app/util/app_version.dart';
 
 class SettingScreen extends StatefulWidget {
   @override
@@ -60,15 +63,20 @@ class _SettingScreenState extends State<SettingScreen> {
               ),
             ),
             Divider(thickness: 1,),
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child:
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text("About MyHealthBD",style: GoogleFonts.poppins(color: HexColor("#333132"),fontSize: 15,fontWeight: FontWeight.w600)),
-                  Text('Want to know more?',style: GoogleFonts.poppins(color: HexColor("#D2D2D2"),fontSize: 11)),
-                ],
+            GestureDetector(
+              onTap: (){
+                Navigator.push(context, MaterialPageRoute(builder: (context)=>AboutUsScreen()));
+              },
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child:
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("About MyHealthBD",style: GoogleFonts.poppins(color: HexColor("#333132"),fontSize: 15,fontWeight: FontWeight.w600)),
+                    Text('Want to know more?',style: GoogleFonts.poppins(color: HexColor("#D2D2D2"),fontSize: 11)),
+                  ],
+                ),
               ),
             ),
             Divider(thickness: 1,),
@@ -96,7 +104,8 @@ class _SettingScreenState extends State<SettingScreen> {
               Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text("Version 1.0.12",style: GoogleFonts.poppins(color: HexColor("#333132"),fontSize: 15,fontWeight: FontWeight.w600)),
+                  AppVersionWidgetSmallForSettings(),
+                  //Text("Version 1.0.12",style: GoogleFonts.poppins(color: HexColor("#333132"),fontSize: 15,fontWeight: FontWeight.w600)),
                  // Text('Choose a unique password',style: GoogleFonts.poppins(color: HexColor("#D2D2D2"),fontSize: 11)),
                 ],
               ),

4f7fac5869db7ac8d8d7f5ddfd1faf8bb1d117a4
shofizone <shofizone@gmail.com>
2021-05-28 03:15:08 +0600

    gradle upgraded

--- build_dev_apk.sh
+++ build_dev_apk.sh
@@ -8,6 +8,7 @@ perl -i -pe 's/^(version:\s+\d+\.\d+\.)(\d+)(\+)(\d+)$/$1.($2+1).$3.($4+1)/e' pu
 version=`grep 'version: ' pubspec.yaml | sed 's/version: //'`
 git commit -m "Version Upgraded to $version" pubspec.yaml
 
-flutter build apk  --flavor dev --target-platform android-arm,android-arm64 --debug
-flutter build apk  --flavor dev --target-platform android-arm,android-arm64 --profile
-flutter build apk  --flavor dev --target-platform android-arm,android-arm64 --release
+flutter build apk  --flavor dev  --debug
+flutter build apk  --flavor dev  --profile
+flutter build apk  --flavor dev --release
+#flutter build apk  --flavor dev --target-platform android-arm,android-arm64 --release

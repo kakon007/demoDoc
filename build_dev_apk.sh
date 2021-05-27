bc1320f119eeab6f611f0d47086405e88a07dfad
shofizone <shofizone@gmail.com>
2021-05-28 02:24:39 +0600

    path issue fixed !

--- build_dev_apk.sh
+++ build_dev_apk.sh
@@ -8,4 +8,5 @@ perl -i -pe 's/^(version:\s+\d+\.\d+\.)(\d+)(\+)(\d+)$/$1.($2+1).$3.($4+1)/e' pu
 version=`grep 'version: ' pubspec.yaml | sed 's/version: //'`
 git commit -m "Version Upgraded to $version" pubspec.yaml
 
+flutter build apk  --flavor dev --target-platform android-arm,android-arm64 --profile -t lib/main.dart
 flutter build apk  --flavor dev --target-platform android-arm,android-arm64 --release -t lib/main.dart

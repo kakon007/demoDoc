3ed1bbd6e9386b92091a4956a3c52cc09c90e75c
Rahat <rahat@ishraak.com>
2021-06-10 18:41:36 +0600

    prod aab sh added

---
+++ build_prod_aab.sh
@@ -0,0 +1,11 @@
+##!/bin/bash
+set -e
+
+# Find and increment the version number.
+perl -i -pe 's/^(version:\s+\d+\.\d+\.\d+\+)(\d+)$/$1.($2+1)/e' pubspec.yaml
+
+# Commit and tag this change.
+version=`grep 'version: ' pubspec.yaml | sed 's/version: //'`
+git commit -m "Version Upgraded to $version" pubspec.yaml
+
+flutter build appbundle --flavor prod --target-platform android-arm,android-arm64,android-x64 --release --tree-shake-icons --split-debug-info --obfuscate -t lib/main.dart

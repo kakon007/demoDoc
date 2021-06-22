7e72f8571c1bc81a7d8bbfed5002b5d8f168c89e
jahid-ish <jahid@ishraak.com>
2021-06-22 11:36:39 +0600

    Document Upload null value can be posted issue solved

--- lib/features/my_health/view/widgets/upload_document_screen.dart
+++ lib/features/my_health/view/widgets/upload_document_screen.dart
@@ -556,64 +556,69 @@ class _UploadDocumentScreenState extends State<UploadDocumentScreen> {
                   padding: const EdgeInsets.only(top: 8.0, left: 10, right: 10),
                   child: writeDetailsField,
                 ),
-                Padding(
-                  padding: const EdgeInsets.only(
-                      top: 15.0, left: 10, right: 10, bottom: 10),
-                  child: InkWell(
-                    onTap: () async {
-                      var accessToken = await Provider.of<AccessTokenProvider>(
-                              appNavigator.context,
-                              listen: false)
-                          .getToken();
-                      var details = Provider.of<UserDetailsViewModel>(
-                          appNavigator.context,
-                          listen: false);
-                      await vm2.uploadDocuments(
-                          file: file == null ? _image : file,
-                          accessToken: accessToken,
-                          attachmentTypeNo: _selectedDocumentType,
-                          pickDate: _reportDate,
-                          regID: details.userDetailsList.id,
-                          username: details.userDetailsList.hospitalNumber,
-                          description: _descriptionTextEditingController.text,
-                        attachmentName: _username.text
-                      );
-                      print("Upload Doc tapped");
-                      // await Future.delayed(Duration(seconds: 3));
-                      // Future.delayed(Duration.zero, () async {
-                      //   setState(() {
-                      //     // file==null && _image==null?Loader():
-                      //     Navigator.of(context)
-                      //         .pushReplacement(MaterialPageRoute(
-                      //             builder: (BuildContext context) =>
-                      //                 // DoctorHomeScreen(
-                      //                 HomeScreen(
-                      //                   accessToken: accessToken,
-                      //                 )));
-                      //   });
-                      // });
-                      Navigator.pop(context);
-                    },
-                    child: Material(
-                      elevation: 2,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(5)),
-                      color: file == null && _image == null ||
-                              _selectedDocumentType == null ||
-                              _reportDate == null
-                          ? HexColor("#B8C2F8")
-                          : HexColor("#354291"),
-                      child: SizedBox(
-                        width: double.infinity,
-                        height:
-                            isTablet? 55 : MediaQuery.of(context).size.width > 600 ? 35 : 40,
-                        child: Center(
-                          child: Text(
-                            "Upload Document",
-                            style: GoogleFonts.poppins(
-                                color: Colors.white,
-                                fontSize: isTablet? 20 : 13,
-                                fontWeight: FontWeight.w500),
+                AbsorbPointer(
+                  absorbing: file == null && _image == null ||
+                      _selectedDocumentType == null ||
+                      _reportDate == null?true:false,
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                        top: 15.0, left: 10, right: 10, bottom: 10),
+                    child: InkWell(
+                      onTap: () async {
+                        var accessToken = await Provider.of<AccessTokenProvider>(
+                                appNavigator.context,
+                                listen: false)
+                            .getToken();
+                        var details = Provider.of<UserDetailsViewModel>(
+                            appNavigator.context,
+                            listen: false);
+                        await vm2.uploadDocuments(
+                            file: file == null ? _image : file,
+                            accessToken: accessToken,
+                            attachmentTypeNo: _selectedDocumentType,
+                            pickDate: _reportDate,
+                            regID: details.userDetailsList.id,
+                            username: details.userDetailsList.hospitalNumber,
+                            description: _descriptionTextEditingController.text,
+                          attachmentName: _username.text
+                        );
+                        print("Upload Doc tapped");
+                        // await Future.delayed(Duration(seconds: 3));
+                        // Future.delayed(Duration.zero, () async {
+                        //   setState(() {
+                        //     // file==null && _image==null?Loader():
+                        //     Navigator.of(context)
+                        //         .pushReplacement(MaterialPageRoute(
+                        //             builder: (BuildContext context) =>
+                        //                 // DoctorHomeScreen(
+                        //                 HomeScreen(
+                        //                   accessToken: accessToken,
+                        //                 )));
+                        //   });
+                        // });
+                        Navigator.pop(context);
+                      },
+                      child: Material(
+                        elevation: 2,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(5)),
+                        color: file == null && _image == null ||
+                                _selectedDocumentType == null ||
+                                _reportDate == null
+                            ? HexColor("#B8C2F8")
+                            : HexColor("#354291"),
+                        child: SizedBox(
+                          width: double.infinity,
+                          height:
+                              isTablet? 55 : MediaQuery.of(context).size.width > 600 ? 35 : 40,
+                          child: Center(
+                            child: Text(
+                              "Upload Document",
+                              style: GoogleFonts.poppins(
+                                  color: Colors.white,
+                                  fontSize: isTablet? 20 : 13,
+                                  fontWeight: FontWeight.w500),
+                            ),
                           ),
                         ),
                       ),

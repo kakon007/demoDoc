2453793dd47cf3a15c8567049a5cf197633e7825
shakil-ish <shakil@ishraak.com>
2021-07-25 14:50:13 +0600

    hospital name and location space

--- lib/features/hospitals/view/widgets/hospitalListCard.dart
+++ lib/features/hospitals/view/widgets/hospitalListCard.dart
@@ -31,9 +31,9 @@ class HospitalListCard extends StatelessWidget {
     bool isTablet = Responsive.isTablet(context);
     bool isMobile = Responsive.isMobile(context);
     return Container(
-      margin: EdgeInsets.only(left: width<330 ? 3 :6, right: width<330 ? 3 :6, bottom: 2),
+      margin: EdgeInsets.only(left: width<330 ? 3 :6, right: width<330 ? 3 :6, bottom: 3),
       width: MediaQuery.of(context).size.width,
-      constraints: BoxConstraints(minHeight: isTablet? 170 : width< 330 ? 118 : 135,),
+    // constraints: BoxConstraints(minHeight: isTablet? 170 : width< 330 ? 118 : 135,),
       //height: isTablet? 170 : width< 330 ? 118 : 135,
       child: Card(
         semanticContainer: true,
@@ -43,7 +43,7 @@ class HospitalListCard extends StatelessWidget {
             Padding(
               padding: const EdgeInsets.all(8.0),
               child: Container(
-                  height: isTablet ? 130 : width<330 ? 80 : 93,
+                  height: isTablet ? 130 : width<330 ? 80 : 85,
                   width: isTablet ? MediaQuery.of(context).size.width*.28 :  111.5,
                   decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(25),
@@ -57,7 +57,7 @@ class HospitalListCard extends StatelessWidget {
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Container(
-                        constraints: BoxConstraints(minHeight: isTablet? 47 : 42,),
+                       // constraints: BoxConstraints(minHeight: isTablet? 47 : 42,),
                         // height: isTablet? 47 : 42,
                         child: Text(titleText.titleCase,maxLines:2,overflow: TextOverflow.ellipsis,style: GoogleFonts.poppins(fontSize: isTablet? 18 :  width<330 ? 10 : 12,fontWeight: FontWeight.bold,),textAlign:TextAlign.start)),
                     SizedBox(height:isTablet? 3 : width<330 ? 1 :  2,),
@@ -117,146 +117,3 @@ class HospitalListCard extends StatelessWidget {
     );
   }
 }
-
-
-
-// import 'package:flutter/material.dart';
-// import 'package:google_fonts/google_fonts.dart';
-// import 'package:hexcolor/hexcolor.dart';
-// import 'package:myhealthbd_app/main_app/resource/colors.dart';
-// import 'package:myhealthbd_app/main_app/resource/strings_resource.dart';
-// class  HospitalListCard extends StatelessWidget {
-//   final String image;
-//   final String hospitalTitle;
-//   final String hospitalLocation;
-//   final String onlineDoctors;
-//   final String buttonName;
-//   HospitalListCard({ this.image, this.buttonName, this.onlineDoctors, this.hospitalLocation, this.hospitalTitle});
-//   @override
-//   Widget build(BuildContext context) {
-//     var cardHeight = MediaQuery.of(context).size.height * 0.1537;
-//     var height = MediaQuery.of(context).size.height;
-//     var cardWidth = MediaQuery.of(context).size.width * 0.3435;
-//     return Container(
-//             height: cardHeight * 1.2,
-//             margin: EdgeInsets.all(8),
-//             decoration: BoxDecoration(
-//               border: Border.all(
-//                 color: HexColor("#D6DCFF"),
-//                 width: 1,
-//               ),
-//               borderRadius: BorderRadius.circular(12),
-//             ),
-//             child: Column(
-//               crossAxisAlignment: CrossAxisAlignment.start,
-//               children: [
-//                 Row(
-//                   //mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//                   children: [
-//                     SizedBox(
-//                       width: height <= 600
-//                           ? cardHeight * 0.09
-//                           : cardHeight * 0.1,
-//                     ),
-//                     Column(
-//                       children: [
-//                         Container(
-//                             width: cardWidth * 0.9,
-//                             height: cardHeight,
-//                             child: Padding(
-//                               padding: const EdgeInsets.all(12.0),
-//                               child: Image.asset(
-//                                 image,
-//                                 fit: BoxFit.cover,
-//                               ),
-//                             )),
-//                       ],
-//                     ),
-//                     SizedBox(
-//                       width: height <= 600
-//                           ? cardHeight * 0.06
-//                           : cardHeight * 0.08,
-//                     ),
-//                     Container(
-//                       width: cardWidth * 1.58,
-//                       height: cardHeight * 1.13,
-//                       decoration: BoxDecoration(
-//                         color: HexColor("#FFFFFF"),
-//                         borderRadius: BorderRadius.only(
-//                             bottomRight: Radius.circular(12),
-//                             topRight: Radius.circular(12)),
-//                       ),
-//                       child: Padding(
-//                         padding: EdgeInsets.only(
-//                             left: 15.0,
-//                             right: 10,
-//                             top: height <= 600 ? 6 : 12),
-//                         child: Column(
-//                           crossAxisAlignment: CrossAxisAlignment.start,
-//                           children: [
-//                             Text(
-//                               hospitalTitle,
-//                               style: GoogleFonts.poppins(
-//                                   fontSize: height <= 600 ? 12 : 15,
-//                                   fontWeight: FontWeight.bold),
-//                             ),
-//                             Text(
-//                               hospitalLocation,
-//                               style: GoogleFonts.poppins(
-//                                 color: Colors.grey,
-//                                 fontSize: height <= 600 ? 9 : 12,
-//                               ),
-//                             ),
-//                             Row(
-//                               children: [
-//                                 Icon(
-//                                   Icons.circle,
-//                                   color: HexColor("#1EE573"),
-//                                   size: height <= 600 ? 10 : 15,
-//                                 ),
-//                                 SizedBox(
-//                                   width: 5,
-//                                 ),
-//                                 Text(
-//                                   onlineDoctors,
-//                                   style: GoogleFonts.poppins(
-//                                     fontSize: height <= 600 ? 9 : 12,
-//                                   ),
-//                                 ),
-//                               ],
-//                             ),
-//                             SizedBox(
-//                               height: height <= 600
-//                                   ? cardHeight / 15
-//                                   : cardHeight / 9,
-//                             ),
-//                             Container(
-//                               width: height <= 600
-//                                   ? cardWidth * 1.2
-//                                   : cardWidth * 1.3,
-//                               child: FlatButton(
-//                                 shape: RoundedRectangleBorder(
-//                                     borderRadius: BorderRadius.circular(8)),
-//                                 color: AppTheme.appbarPrimary,
-//                                 onPressed: () {},
-//                                 textColor: Colors.white,
-//                                 child: Text(
-//                                   buttonName,
-//                                   style: GoogleFonts.poppins(
-//                                       fontSize: height <= 600 ? 8 : 10,
-//                                       color: Colors.white,
-//                                       fontWeight: FontWeight.w600),
-//                                 ),
-//                               ),
-//                             )
-//                           ],
-//                         ),
-//                       ),
-//                     )
-//                   ],
-//                 ),
-//               ],
-//             ),
-//           );
-//   }
-// }

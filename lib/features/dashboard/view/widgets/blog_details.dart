a49ee6a6ca75b68b166e115edf03b14b87547e5e
shofizone <shofizone@gmail.com>
2021-08-15 01:18:13 +0600

    some txt overflow issue fixed.

--- lib/features/dashboard/view/widgets/blog_details.dart
+++ lib/features/dashboard/view/widgets/blog_details.dart
@@ -1,37 +1,27 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_html/flutter_html.dart';
-import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:webview_flutter/webview_flutter.dart';
 
-class BlogDetails extends StatefulWidget {
+class BlogDetails extends StatelessWidget {
   final String details;
   final String title;
   BlogDetails({this.details, this.title});
 
   @override
-  _BlogDetailsState createState() => _BlogDetailsState();
-}
-
-class _BlogDetailsState extends State<BlogDetails> {
-  WebViewController _controller;
-
-  @override
   Widget build(BuildContext context) {
-    return WebviewScaffold(
+    return Scaffold(
       appBar: AppBar(
         title: Text(
-          widget.title,
+          title,
           style: GoogleFonts.poppins(fontSize: 14),
         ),
       ),
-      url: Uri.dataFromString(
-        widget.details,
-        mimeType: 'text/html',
-        encoding: Encoding.getByName('utf-8'),
-      ).toString(),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        child: Html(
+          data: details,
+        ),
+      ),
     );
   }
 }

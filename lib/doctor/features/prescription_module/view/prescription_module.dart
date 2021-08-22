9013110b72096f70dc94157dcb0ace4d6ea3305f
akramuzzaman <akram@ishraak.com>
2021-08-22 13:05:12 +0600

    card added prescription module

--- lib/doctor/features/prescription_module/view/prescription_module.dart
+++ lib/doctor/features/prescription_module/view/prescription_module.dart
@@ -1,5 +1,9 @@
+import 'package:expandable/expandable.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'dart:math' as math;
+
+import 'package:myhealthbd_app/doctor/features/prescription_module/view/widgets/prescription_common_widget.dart';
 
 class Module extends StatefulWidget {
   @override
@@ -9,6 +13,24 @@ class Module extends StatefulWidget {
 class _ModuleState extends State<Module> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text("Prescription Module"),),);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Prescription Module"),
+      ),
+      body: ExpandableTheme(
+        data: const ExpandableThemeData(
+          tapHeaderToExpand: true,
+          iconColor: Colors.blue,
+          useInkWell: false,
+        ),
+        child: ListView(
+          physics: const BouncingScrollPhysics(),
+          children: <Widget>[
+            PrescriptionCommonWidget(),
+            // Card3(),
+          ],
+        ),
+      ),
+    );
   }
 }
---
+++ lib/doctor/features/prescription_module/view/widgets/prescription_common_widget.dart
@@ -0,0 +1,80 @@
+import 'package:expandable/expandable.dart';
+import 'package:flutter/material.dart';
+import 'dart:math' as math;
+
+import 'package:myhealthbd_app/main_app/resource/colors.dart';
+
+class PrescriptionCommonWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    buildItem(String label) {
+      return Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Text(label),
+      );
+    }
+
+    buildList() {
+      return Column(
+        children: <Widget>[
+          for (var i in [1, 2, 3, 4]) buildItem("Item ${i}"),
+        ],
+      );
+    }
+
+    return ExpandableNotifier(
+        initialExpanded: true,
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: ScrollOnExpand(
+            child: Card(
+              clipBehavior: Clip.antiAlias,
+              child: Column(
+                children: <Widget>[
+                  ExpandablePanel(
+                    theme: const ExpandableThemeData(
+                      headerAlignment: ExpandablePanelHeaderAlignment.center,
+                      tapBodyToExpand: true,
+                      tapBodyToCollapse: true,
+                      hasIcon: false,
+                    ),
+                    header: Container(
+                      color: AppTheme.buttonActiveColor,
+                      child: Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Row(
+                          children: [
+                            Expanded(
+                              child: Text(
+                                "Items",
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .bodyText1
+                                    .copyWith(color: Colors.white),
+                              ),
+                            ),
+                            ExpandableIcon(
+                              theme: const ExpandableThemeData(
+                                expandIcon: Icons.keyboard_arrow_down_outlined,
+                                collapseIcon: Icons.keyboard_arrow_up_outlined,
+                                iconColor: Colors.white,
+                                iconSize: 28.0,
+                                iconRotationAngle: math.pi / 2,
+                                iconPadding: EdgeInsets.only(right: 5),
+                                hasIcon: false,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    collapsed: Container(),
+                    expanded: buildList(),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ));
+  }
+}

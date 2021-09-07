2ac3ec38357121f30a5e3129398ba549969dcda5
jahid-ish <jahid@ishraak.com>
2021-09-07 12:56:00 +0600

    Appointment report screen issue solved

--- lib/doctor/features/appointment_report/repositories/appointment_report_repository.dart
+++ lib/doctor/features/appointment_report/repositories/appointment_report_repository.dart
@@ -5,10 +5,11 @@ import 'package:myhealthbd_app/main_app/api_client.dart';
 import 'package:myhealthbd_app/main_app/failure/app_error.dart';
 
 class AppointmentReportHistory{
-  Future<Either<AppError,ConsModel>> fetchAppointmentReportList({int doctorNo,int ogNo, var fromDate, var toDate}) async {
+  Future<Either<AppError,ConsModel>> fetchAppointmentReportList({int doctorNo,int ogNo, var fromDate, var toDate,int shiftNo}) async {
     try{
-      var response=await ApiClient().postRequest("diagnostic-api/api/opd-appointment-report/report-list", {"fromDate":"$fromDate","toDate":"$toDate","printFormate":"PDF","doctorNo":doctorNo,"ogNo":ogNo,"shiftNo":0});
+      var response=await ApiClient().postRequest("diagnostic-api/api/opd-appointment-report/report-list", {"fromDate":"$fromDate","toDate":"$toDate","printFormate":"PDF","doctorNo":doctorNo,"ogNo":ogNo,"shiftNo":shiftNo});
       print('constwer ${response.statusCode}');
+      print('constwe $shiftNo');
       if(response.statusCode==200){
         var body=response.body;
         AppointmentReportListModel data=appointmentReportListModelFromJson(body);

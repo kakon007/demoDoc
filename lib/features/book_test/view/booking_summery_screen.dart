86df24f844286cd157023689bca0435681d84e1a
jahid-ish <jahid@ishraak.com>
2021-09-08 11:55:36 +0600

    book summery UI design

--- lib/features/book_test/view/booking_summery_screen.dart
+++ lib/features/book_test/view/booking_summery_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:hexcolor/hexcolor.dart';
 import 'package:myhealthbd_app/main_app/util/responsiveness.dart';
 
 class BookingSummeryScreen extends StatefulWidget {
@@ -33,161 +35,204 @@ class _BookingSummeryScreenState extends State<BookingSummeryScreen> {
             ),
           ],
         ),
-        body: Column(
-          children: [
-            Container(
-              height: 60,
-              width: double.infinity,
-              decoration: new BoxDecoration(
-                color: Colors.white,
-                borderRadius: new BorderRadius.only(
-                  bottomLeft: const Radius.circular(12.0),
-                  bottomRight: const Radius.circular(12.0),
-                ),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey.withOpacity(0.2),
-                    spreadRadius: 5,
-                    blurRadius: 5,
-                    offset: Offset(0, 1), // changes position of shadow
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                height: 60,
+                width: double.infinity,
+                decoration: new BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: new BorderRadius.only(
+                    bottomLeft: const Radius.circular(12.0),
+                    bottomRight: const Radius.circular(12.0),
                   ),
-                ],
-              ),
-              child: Padding(
-                padding: const EdgeInsets.only(left: 25.0,right: 25),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      'Items  (8)',
-                      style: GoogleFonts.poppins(
-                          fontSize: isTablet ? 20 : 15,fontWeight: FontWeight.w600, color: Color(0xff354291)),
-                    ),
-                    Text(
-                      'Price  (8)',
-                      style: GoogleFonts.poppins(
-                          fontSize: isTablet ? 20 : 15, fontWeight: FontWeight.w600, color: Color(0xff354291)),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.2),
+                      spreadRadius: 5,
+                      blurRadius: 5,
+                      offset: Offset(0, 1), // changes position of shadow
                     ),
                   ],
                 ),
-              ),
-            ),
-           SizedBox(
-             height: 10,
-           ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: ListView.separated(
-                  itemCount: 8,
-                  shrinkWrap: true,
-                  itemBuilder:
-                      (BuildContext context,
-                      int index) {
-                    return Padding(
-                      padding:
-                      const EdgeInsets.only(
-                          left: 10.0,
-                          right: 10),
-                      child: Container(
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Container(
-                              width:260,
-                              child: Text(
-                                'O Cell (PRBC) Suspended in AB Plasma',
-                                style: GoogleFonts.poppins(
-                                    fontSize: isTablet ? 20 : 14, color: Colors.black),
-                                maxLines: 2,overflow: TextOverflow.ellipsis,
-                              ),
-                            ),
-
-                            Text(
-                              '12,000.00',
-                              style: GoogleFonts.poppins(
-                                  fontSize: isTablet ? 20 : 15, color: Colors.black),
-                            ),
-                            Icon(Icons.delete)
-                          ],
-                        )
-                      ),
-                    );
-                  },
-                  separatorBuilder:
-                      (context, index) {
-                    return Divider(thickness: 2,);
-                  }),
-            ),
-
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Container(
-                decoration: new BoxDecoration(
-                  color: Colors.white,
-                  borderRadius:  BorderRadius.only(
-                    bottomLeft:  Radius.circular(12.0),
-                    bottomRight:  Radius.circular(12.0),
-                    topLeft: Radius.circular(12.0),
-                    topRight: Radius.circular(12.0),
-                  ),
-                    border: Border.all(color: Colors.grey)
-              ),
                 child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
+                  padding: const EdgeInsets.only(left: 25.0,right: 25),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            'Subtotal',
-                            style: GoogleFonts.poppins(
-                                fontSize: isTablet ? 20 : 15,fontWeight: FontWeight.w600, color: Colors.black),
-                          ),
-                          Text(
-                            '31,210.00 Tk',
-                            style: GoogleFonts.poppins(
-                                fontSize: isTablet ? 20 : 15, fontWeight: FontWeight.w600, color: Colors.black),
-                          ),
-                        ],
+                      Text(
+                        'Items  (8)',
+                        style: GoogleFonts.poppins(
+                            fontSize: isTablet ? 20 : 15,fontWeight: FontWeight.w600, color: Color(0xff354291)),
                       ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            'Discount',
-                            style: GoogleFonts.poppins(
-                                fontSize: isTablet ? 20 : 15,fontWeight: FontWeight.w600, color: Color(0xff037BB7)),
-                          ),
-                          Text(
-                            '1,526.00 Tk',
-                            style: GoogleFonts.poppins(
-                                fontSize: isTablet ? 20 : 15, fontWeight: FontWeight.w600, color: Color(0xff037BB7)),
-                          ),
-                        ],
-                      ),
-                      Divider(),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            'Total',
-                            style: GoogleFonts.poppins(
-                                fontSize: isTablet ? 20 : 15,fontWeight: FontWeight.w600, color: Colors.black),
-                          ),
-                          Text(
-                            '29,684.00 Tk',
-                            style: GoogleFonts.poppins(
-                                fontSize: isTablet ? 20 : 15, fontWeight: FontWeight.w600, color: Colors.black),
-                          ),
-                        ],
+                      Text(
+                        'Price  (8)',
+                        style: GoogleFonts.poppins(
+                            fontSize: isTablet ? 20 : 15, fontWeight: FontWeight.w600, color: Color(0xff354291)),
                       ),
                     ],
                   ),
                 ),
               ),
-            )
-          ],
+             SizedBox(
+               height: 10,
+             ),
+              Container(
+                height: 450,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ListView.separated(
+                      itemCount: 8,
+                      shrinkWrap: true,
+                      itemBuilder:
+                          (BuildContext context,
+                          int index) {
+                        return Padding(
+                          padding:
+                          const EdgeInsets.only(
+                              left: 10.0,
+                              right: 10),
+                          child: Container(
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Container(
+                                  width:260,
+                                  child: Text(
+                                    'O Cell (PRBC) Suspended in AB Plasma',
+                                    style: GoogleFonts.poppins(
+                                        fontSize: isTablet ? 20 : 14, color: Colors.black),
+                                    maxLines: 2,overflow: TextOverflow.ellipsis,
+                                  ),
+                                ),
+
+                                Text(
+                                  '12,000.00',
+                                  style: GoogleFonts.poppins(
+                                      fontSize: isTablet ? 20 : 15, color: Colors.black),
+                                ),
+                                Icon(Icons.delete_sweep,
+                                    size: isTablet ? 27 : 20,
+                                    color:Color(0xffFFA7A7)),
+                              ],
+                            )
+                          ),
+                        );
+                      },
+                      separatorBuilder:
+                          (context, index) {
+                        return Divider(thickness: 2,);
+                      }),
+                ),
+              ),
+
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Container(
+                  decoration: new BoxDecoration(
+                    color: Colors.white,
+                    borderRadius:  BorderRadius.only(
+                      bottomLeft:  Radius.circular(12.0),
+                      bottomRight:  Radius.circular(12.0),
+                      topLeft: Radius.circular(12.0),
+                      topRight: Radius.circular(12.0),
+                    ),
+                      border: Border.all(color: Colors.grey.withOpacity(0.2))
+                ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              'Subtotal',
+                              style: GoogleFonts.poppins(
+                                  fontSize: isTablet ? 20 : 14, color: Colors.black),
+                            ),
+                            Text(
+                              '31,210.00 Tk',
+                              style: GoogleFonts.poppins(
+                                  fontSize: isTablet ? 20 : 14, color: Colors.black),
+                            ),
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              'Discount',
+                              style: GoogleFonts.poppins(
+                                  fontSize: isTablet ? 20 : 14, color: Color(0xff037BB7)),
+                            ),
+                            Text(
+                              '1,526.00 Tk',
+                              style: GoogleFonts.poppins(
+                                  fontSize: isTablet ? 20 : 14, color: Color(0xff037BB7)),
+                            ),
+                          ],
+                        ),
+                        Divider(thickness: 1.5,color: Colors.black,),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              'Total',
+                              style: GoogleFonts.poppins(
+                                  fontSize: isTablet ? 20 : 14, color: Colors.black),
+                            ),
+                            Text(
+                              '29,684.00 Tk',
+                              style: GoogleFonts.poppins(
+                                  fontSize: isTablet ? 20 : 14, color: Colors.black),
+                            ),
+
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              // SizedBox(
+              //   height: 15,
+              // ),
+
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: GestureDetector(
+                  onTap: () {
+
+                  },
+                  child: Material(
+                    elevation: 2,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5)),
+                    color: HexColor("#354291"),
+                    child: SizedBox(
+                      // width: MediaQuery.of(context).size.width > 600
+                      //     ? 335
+                      //     : 300,
+                      height: MediaQuery.of(context).size.width > 600
+                          ? 35
+                          : 45,
+                      child: Center(
+                        child: Text(
+                          "Confirm Booking",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 15,
+                              fontWeight: FontWeight.w500),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );

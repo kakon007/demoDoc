4cbf12c8f50f2b5308d3c0c80a3f8854a59098b2
shakil <shakil@ishraak.com>
2021-09-13 18:07:24 +0600

    patient user details ui design

---
+++ lib/admin/profile/admin_profile.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class AdminProfile extends StatefulWidget {
+  @override
+  _AdminProfileState createState() => _AdminProfileState();
+}
+
+class _AdminProfileState extends State<AdminProfile> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Profile'),),
+    );
+  }
+}
